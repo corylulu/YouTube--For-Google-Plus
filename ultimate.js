bbc6ff8e401a19e5dbc38ad0f68238e32edc07a5
Corylulu <corylulu@gmail.com>
2011-12-29 12:26:57 -0800

    first commit

---
+++ ultimate.js
@@ -0,0 +1,157 @@
+/*Css class names from Google Plus (likely to change over time with updates, keep here to make easier updating.)*/
+var body_className_isMainPage = 'ULpcLb';
+var a_className_isTitleLink = 'B-u-Y-j';
+var div_className_isDescLinkParent = 'B-u-wa-ea';
+var a_className_userLink = 'ot-anchor';
+
+
+/*Created Elements that are used in multiple functions*/
+var youTubeBox = document.createElement('div');
+var iframe = document.createElement('iframe');
+
+/*Keeps a count of found links on page*/
+var currentCount = 0;
+
+initialize();
+setInterval(function()
+{ 
+	/*Cross checks currentCount with any new links found, if changed, call placeLinks()*/
+	var Links = document.getElementsByClassName(a_className_userLink);
+	if(currentCount != Links.length)
+		placeLinks();
+}, 2000);
+
+function initialize()
+{ 
+	var gplusBody = document.getElementsByClassName(body_className_isMainPage)[0];
+	/*Checks for body class name to ensure you're on a Google+ content page*/
+	if(gplusBody !== undefined)
+	{
+		var sheet = document.createElement('style');
+		/*creates the CSS that is used by this extension*/
+		sheet.innerHTML = 	".cllFrame{width:100%;height:286px;bottom: 0px; border: #777 1px solid} "+
+							".cll {z-index: 2; position: fixed; overflow-x: hidden; overflow-y: hidden; left: 10px; "+
+									"bottom: 0px; width: 400px; height: 30px;} "+
+							".cllHead {background-color:#484848;width:170px; float:left; height:30px;vertical-align:center; cursor: pointer;} "+ 
+							".cllText{color:white; font:bold 13px/30px arial,sans-serif; margin: auto 0px; padding-top:1px; padding-left:10px;text-align:center; cursor: pointer; float:left;} "+
+							".cllLinks {color:red; font-weight:bold; padding-right: 5px;cursor: pointer;} "+
+							".cllLinksVmid { vertical-align: middle;}"+
+							".title_widget_button { height:24px; width: 24px; margin-top: 5px; margin-bottom: 0px; vertical-align:center; -webkit-appearance: none; padding: 0; margin-right: 3px; overflow: hidden; cursor: pointer; background-color: transparent; border: 0px !important;} .hoverOutline :hover{background-color: #777;}"+
+							".fRight{float:right;margin-right: 7px !important;}"+
+							".fLeft{float:left;}";
+		/*Appends css to page body*/
+		document.body.appendChild(sheet);
+
+		/*Creates the container div (initialized at higher level)*/
+		youTubeBox.className  = "cll";
+		youTubeBox.id = "cllDiv";
+		/*Creates attribute to indicate that no link is bound to the iframe (for disabling toggle)*/
+		youTubeBox.setAttribute('cllState', "false");
+		/*Appends container div to pages body*/
+		gplusBody.appendChild(youTubeBox);
+		
+		/*Creates the header div*/
+		var youTubeBoxHead = document.createElement('div');
+		youTubeBoxHead.className  = "cllHead";
+		
+		/*Places icon, text and minimize button*/
+		youTubeBoxHead.innerHTML = '<p class="cllText"><button id="mn" class="title_widget_button fLeft" style="vertical-align:bottom !important;" title="Minimize" aria-label="Minimize">' + "<img src='"+chrome.extension.getURL("youtubeicon.png")+"'></button>YouTube+ </p>" + '<button id="mn" class="title_widget_button hoverOutline minimize fRight" title="Minimize" aria-label="Minimize">  <img src="https://talkgadget.google.com/talkgadget/resources/minimize.png" class="icon  minimize "> </button>';
+		
+		/*Sets the onclick to Expand/Collapse*/
+		youTubeBoxHead.onclick = function(){ this.parentNode.style.height=((this.parentNode.style.height=='30px')&&(this.parentNode.getAttribute('cllState') !== "false"))? '316px': '30px';};
+		
+		/*Appends header div to container div*/
+		youTubeBox.appendChild(youTubeBoxHead);
+
+		/*Creates the iframe div (initialized at higher level)*/
+		iframe.name = "cllFrame";
+		iframe.className  = "cllFrame";
+		iframe.id = "cllFrame";
+		/*Appends iframe to container div*/
+		youTubeBox.appendChild(iframe);
+		
+		
+	}
+}
+
+function placeLinks()
+{
+		var Links = document.getElementsByClassName(a_className_userLink); /*Finds all embeded links*/
+		
+		for(var i = 0; i < Links.length; i++)
+		{
+			if(Links[i].previousSibling) /*check null*/
+			{
+				if (Links[i].previousSibling.className === undefined){/*checks undefined className - do nothing if-so*/}
+				else if (Links[i].previousSibling.className.indexOf("cllLinks") != -1) 
+					continue;  
+					/*Skips because "YouTube+" button already exists */
+			}
+			if(Links[i].parentNode.className == div_className_isDescLinkParent)
+				continue; /*Skip links that are in descriptions of embeded videos*/
+				
+			var newField = document.createElement('a');
+			var embedLoc = ""; /*Stores full location of embed-form link*/
+			if(Links[i].href.indexOf("http://www.youtube.com/watch?v=") != -1)
+			{
+				embedLoc = "http://www.youtube.com/embed/" + Links[i].href.replace("http://www.youtube.com/watch?v=", "");
+				/*Found googles full Youtube Link*/
+			}	
+			else if(Links[i].href.indexOf("http://youtu.be/") != -1)
+			{
+				var embedLoc = "http://www.youtube.com/embed/" + Links[i].href.replace("http://youtu.be/", "");
+				/*Found googles short-hand Youtube Link*/
+			}
+			else
+			{
+				continue; 
+				/*Doesn't find youtube link, continues through loop.*/
+			}
+			
+			/*Creates "YouTube+" link next to existing YouTube link.*/
+			var newField = document.createElement('a'); 
+			
+			/*Stores location in link*/
+			newField.setAttribute('cll', embedLoc); 
+			
+			/*Checks to see if text is Title text (has different text formatting css)*/
+			if(Links[i].className.indexOf(a_className_isTitleLink) != -1)
+				newField.className = "cllLinks cllLinksVmid";
+			else
+				newField.className = "cllLinks";
+				
+			/*Force expances the YouTube+ widget in the bottom-left corner onclick and sets the src*/	
+			newField.onclick = function(){toggle(this)};
+			/*Sets link target to the iframe (shouldn't actually do anything)*/
+			newField.target = "cllFrame";
+			/*Sets text*/
+			newField.innerHTML ="<b>YouTube+</b>";
+			/*Inserts my link directly before previous YouTube link*/
+			Links[i].parentNode.insertBefore(newField, Links[i]);
+			
+		}
+		/*Keeps track of how many links were found to cross check when links are rescanned in the top setInterval() */
+		currentCount = Links.length;
+}
+
+function toggle(element)
+{ 
+	var newLink = element.getAttribute('cll'); 
+	d_nested = youTubeBox.firstChild.nextSibling;
+	youTubeBox.removeChild(d_nested); 
+	
+	/*Recreates the iframe because I was having issues with the iframe not loading by just changing the src.*/
+	var newIframe = document.createElement('iframe');
+	newIframe.name = "cllFrame";
+	newIframe.className  = "cllFrame";
+	newIframe.id = "cllFrame";
+	newIframe.setAttribute('src', newLink);
+	youTubeBox.appendChild(newIframe);
+	
+	/*Expand the bottom left area to show iframe*/
+	var oYouTubeBox = document.getElementById("cllDiv");
+	oYouTubeBox.style.height='316px';
+	oYouTubeBox.setAttribute('cllState', "true");
+}
+
+
